c88e7f034099f8e566478a23e30f31b1ba5cf737
leonardo <Leonardo.Azevedo@fgv.br>
2024-05-02 19:16:33 -0300

    função simples

---
+++ 01jornada/teste8.py
@@ -0,0 +1,23 @@
+
+
+x="!"
+def  myfucion():
+    global z
+    z="isso"
+    print("incrivel",x ,z)
+myfucion()  
+
+print("até que é legal",x,z)
+
+a=2j
+print(type(a))
+
+
+x= int(2.8)
+y=float(4)
+z=complex(y)
+
+print(x,y,z)
+
+print(type(x),type(y),type(z))
+
