b2b713d2369ce3e3b3ed6dac58a4a8d6181be8a7
leonardo <Leonardo.Azevedo@fgv.br>
2024-05-03 14:23:57 -0300

    fatiar strings

---
+++ 01jornada/fatiarsrings.py
@@ -0,0 +1,23 @@
+
+txt="seja bem vindo"
+x="seja" in txt
+print(x)
+print("carro " in txt)
+
+if "seja" in txt: # ou poderia ser x
+    print("esta presente")
+
+if "oi" not in txt:
+    print("não esta presente")
+
+
+nome="gabriel artigas"
+print(nome[2:5])
+print(nome[0:7])
+print(nome[8:15])
+print(nome[0:15])
+print(nome[-5:-2])#(-)começa ao contrario
+
+fatianome=nome[3:9]
+print(fatianome)
+
