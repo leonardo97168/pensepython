630a8379296ed69b7a38ca10185a1be2656b2de6
leonardo <Leonardo.Azevedo@fgv.br>
2024-05-06 07:20:13 -0300

    basicos

---
+++ 01jornada/exercicios2.py
@@ -0,0 +1,17 @@
+
+
+y="legal"
+z="incrivel"
+def myfunc ():
+   global x 
+   x="é"
+   print("python ",x ,y)
+myfunc()
+print("você ",x)
+
+num1=input("digite um numero: ")
+
+num2=input("digite um numero: ")
+
+pro=int(num1)*int(num2)
+print(pro)
